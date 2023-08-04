533e3c0cd425c4dd732b1baa9b92987321bb6783
CongChuong <92925332+CongChuong1909@users.noreply.github.com>
2023-08-04 12:19:29 +0700

    fix conflit mui version

--- Client/src/axios.js
+++ Client/src/axios.js
@@ -1,10 +1,21 @@
+// import axios from "axios";
+// export const makeRequest = axios.create({
+//   baseURL: "http://localhost:4000/api/",
+//   withCredentials: true,
+// });
+// export const makeRequestAuth = axios.create({
+//     baseURL: "http://localhost:4000/",
+//     withCredentials: true,
+//   });
+
+
 import axios from "axios";
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "http://52.62.206.125:4000/api/",
   withCredentials: true,
 });
 export const makeRequestAuth = axios.create({
-    baseURL: "http://localhost:4000/",
+    baseURL: "http://52.62.206.125:4000/",
     withCredentials: true,
   });
 
